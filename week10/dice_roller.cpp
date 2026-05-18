cc1339e12888b69bb7ea96cfb381332f92c8140c
Lucas Moraes <lucasmo066@gmail.com>
2026-05-17 21:36:35 -0600

    roll die test program

---
+++ week10/dice_roller.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int rollDie() {
+    return 0; //stub
+}
+
+int getHighest(int rolls[], int count) {
+    return 0; //stub
+}
+
+int main() {
+    srand(time(0));
+
+    return 0;
+}
