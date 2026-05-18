7679d300088d69e68d43b53bda8a73f0f8ce7b38
Lucas Moraes <lucasmo066@gmail.com>
2026-05-17 21:48:58 -0600

    added highest roll

--- week10/dice_roller.cpp
+++ week10/dice_roller.cpp
@@ -4,15 +4,34 @@
 using namespace std;
 
 int rollDie() {
-    return 0; //stub
+    return (rand() % 6) + 1; 
 }
 
 int getHighest(int rolls[], int count) {
-    return 0; //stub
+    int highest = rolls[0];
+    for (int i = 1; i < count; i++) {
+        if (rolls[i] > highest) {
+            highest = rolls[i];
+        }
+    }
+    return highest;
 }
 
 int main() {
     srand(time(0));
-
+    
+    int numRolls;
+    cout << "How many times do you want to roll? ";
+    cin >> numRolls;
+    
+    int rolls[numRolls];
+    
+    for (int i = 0; i < numRolls; i++) {
+        rolls[i] = rollDie();
+        cout << "Roll " << i + 1 << ": " << rolls[i] << endl;
+    }
+    
+    cout << "Highest roll: " << getHighest(rolls, numRolls) << endl;
+    
     return 0;
 }
